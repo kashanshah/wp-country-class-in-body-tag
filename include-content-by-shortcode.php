19ecc0370ec324ead785ac0232cb8e8c44d9b414
kashanshah <kashanshah@hotmail.com>
2018-05-26 02:23:50 +0500

    Condition added to support id and slug of included text posts only

--- include-content-by-shortcode.php
+++ include-content-by-shortcode.php
@@ -8,22 +8,23 @@ Version: 0.0.9
 Author URI: http://www.kashanshah.ga
 */
 
+//Adding Plugin Menu
+add_action('admin_menu', 'icbscbks_admin_actions');
+function icbscbks_admin_actions() {
+    add_submenu_page('edit.php?post_type=icbscbks', 'Settings', 'Settings', 'manage_options', 'include-content-by-shortcode', 'icbscbks_admin'); 
+}
+// Including Admin Panel Settings Page
 function icbscbks_admin() {
     include('admin_view.php');
 }
-function icbscbks_admin_actions() {
-    global $icbscbks_settings_page;
-
-      $icbscbks_settings_page = add_submenu_page('edit.php?post_type=icbscbks', 'Settings', 'Settings', 'manage_options', 'include-content-by-shortcode', 'icbscbks_admin'); 
-}
-add_action('admin_menu', 'icbscbks_admin_actions');
 
 
-// Our custom post type function
+
+
+// Creating custom post type for storing Shortcode Content
+add_action( 'init', 'icbscbks_create_posttype' );
 function icbscbks_create_posttype() {
- 
-    register_post_type( 'icbscbks',
-    // CPT Options
+     register_post_type( 'icbscbks',
         array(
             'labels' => array(
                 'name' => __( 'Included Contents' ),
@@ -46,27 +47,26 @@ function icbscbks_create_posttype() {
         )
     );
 }
-add_action( 'init', 'icbscbks_create_posttype' );
 
-// Add the custom columns to the icbscbks post type:
+
+// Adding shortcode column to the icbscbks post type:
 add_filter( 'manage_icbscbks_posts_columns', 'icbscbks_set_custom_edit_icbscbks_columns' );
 function icbscbks_set_custom_edit_icbscbks_columns($columns) {
     $columns['Shortcode'] = 'Shortcode';
     return $columns;
 }
 
-// Add the data to the custom columns for the icbscbks post type:
+// Add the data to the shortcode column for the icbscbks post type:
 add_action( 'manage_icbscbks_posts_custom_column' , 'icbscbks_custom_icbscbks_column', 10, 2 );
 function icbscbks_custom_icbscbks_column( $column, $post_id ) {
     switch ( $column ) {
-
         case 'Shortcode' :
-                echo '<span class="icbscbks_shortcode"><input style="background: inherit;color: inherit;font-size: 16px;width:100%;padding: 4px 8px;margin: 0;" type="text" value="[includedcontentbyshortcode id='.$post_id.']" readonly onfocus="this.select();" /></span>';
+            echo '<span class="icbscbks_shortcode"><input style="background: inherit;color: inherit;font-size: 16px;width:100%;padding: 4px 8px;margin: 0;" type="text" value="[includedcontentbyshortcode id='.$post_id.']" readonly onfocus="this.select();" /></span>';
             break;
-
     }
 }
 
+// Reading Shortcode From Content
 function icbscbks_func( $atts ) {
     $icbscbks_scAttr = shortcode_atts( array(
         'slug' => 'something',
@@ -76,17 +76,25 @@ function icbscbks_func( $atts ) {
     // [icbscbks id=123]
     if($icbscbks_scAttr['id'] != ""){
         $icbscbks_post = get_post($icbscbks_scAttr['id']);
-        $icbscbks_postcontent = $icbscbks_post->post_content;
-        $icbscbks_postcontent = apply_filters('the_content', $icbscbks_postcontent);
+        if($icbscbks_post->post_type == 'icbscbks'){
+            $icbscbks_postcontent = $icbscbks_post->post_content;
+            $icbscbks_postcontent = apply_filters('the_content', $icbscbks_postcontent);
+        }else{
+            $icbscbks_postcontent = '';
+        }
     }
     // [icbscbks slug=included-content-one]
     else if($icbscbks_scAttr['slug'] != ""){
         $icbscbks_post = get_page_by_path( $icbscbks_scAttr['slug'], OBJECT, 'icbscbks' );
-        $icbscbks_postcontent = $icbscbks_post->post_content;
-        $icbscbks_postcontent = apply_filters('the_content', $icbscbks_postcontent);
-    }
-    else if($icbscbks_scAttr['slug'] != ""){
-        $icbscbks_postcontent = '';
+        if($icbscbks_post->post_type == 'icbscbks'){
+            $icbscbks_postcontent = $icbscbks_post->post_content;
+            $icbscbks_postcontent = apply_filters('the_content', $icbscbks_postcontent);
+        }else{
+            $icbscbks_postcontent = '';
+        }
+}
+    else{
+        $icbscbks_postcontent = '<!-- Please add any slug or id to shortcode --> ';
     }
 
     return $icbscbks_postcontent;
